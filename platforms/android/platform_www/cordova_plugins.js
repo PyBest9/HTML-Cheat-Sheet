8654be8b8ba72f6578850bb37dc7308c010869e8
PyBest9 <radeondz2@gmail.com>
2019-11-15 12:08:15 +0100

    update upto 15.11.2019

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-admobpro.AdMob",
+      "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+      "pluginId": "cordova-plugin-admobpro",
+      "clobbers": [
+        "window.AdMob"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-extension": "1.5.4",
+    "cordova-plugin-admobpro": "2.49.0"
+  };
+});
